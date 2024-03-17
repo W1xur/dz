183cc267d5cdb5820acd980a84f48492fb8af0e8
Rayz0N <Rayz0N>
2024-03-17 15:45:01 +0300

    18.03

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double inp;
+    int res;
+    cout << "Введите вещественное число: ";
+    cin >> inp;
+    res = (int)inp;
+    cout << res;
+
+}
